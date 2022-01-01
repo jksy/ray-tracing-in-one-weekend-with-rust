4801deb57d867fc646220819e66ff526d5dead62
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2022-01-01 13:26:58 +0900

    rendering a surface on a sphere

--- src/raytrace/mod.rs
+++ src/raytrace/mod.rs
@@ -129,27 +129,34 @@ impl Ray {
         }
     }
 
-    pub fn at(self, t: f64) -> Point3 {
+    pub fn at(&self, t: f64) -> Point3 {
         let origin = self.origin;
         return origin + self.direction * t;
     }
 
     pub fn color(&self) -> Color {
-        if self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5) {
-            return Color::new(1.0, 0.0, 0.0);
+        let t =  self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5);
+        if t > 0.0 {
+            let at = self.at(t);
+            let n = at - Vec3::new(0.0, 0.0, -1.0);
+            return Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
         }
         let unit_direction = self.direction.unit_vector();
-        let t = unit_direction.y() + 1.0f64;
-        return Color::new(1.0, 1.0, 1.0) * (1.0f64 - t)  + Color::new(0.5*1.0, 0.7*1.0, 1.0*1.0) * t;
+        let t = unit_direction.y() + 1.0;
+        return Color::new(1.0, 1.0, 1.0) * (1.0 - t)  + Color::new(0.5*1.0, 0.7*1.0, 1.0*1.0) * t;
     }
 
-    fn hit_sphere(&self, center: Point3, radius: f64) -> bool {
+    fn hit_sphere(&self, center: Point3, radius: f64) -> f64 {
         let oc = self.origin - center;
         let a = self.direction.dot(self.direction);
         let b = 2.0 * oc.dot(self.direction);
         let c = oc.dot(oc) - radius * radius;
         let discriminant = b * b - 4.0 * a * c;
-        return discriminant > 0.0;
+        if discriminant < 0.0 {
+            return -1.0;
+        } else {
+            return (-b - discriminant.sqrt() ) / ( 2.0 * a );
+        }
     }
 }
 
